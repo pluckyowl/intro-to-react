69724da609a1eb238eb049acf206deefa962f6bc
Meghan Vidal <meghan.vidal@yahoo.com>
2018-12-17 23:19:21 -0800

    React-to-do: Components

---
+++ react-to-do/src/components/ToDo.js
@@ -0,0 +1,11 @@
+ import React, { Component } from 'react';
+
+ class ToDo extends Component {
+   render() {
+     return (
+       <li> A to-do will go here </li>
+     );
+   }
+ }
+
+ export default ToDo;
